6568b4a8ceddce3130a2407cdb4d3ac2b567608d
mahesh <maheshryali@gmail.com>
2022-12-03 13:10:55 +0530

    changes1

--- loadbalancer.groovy
+++ loadbalancer.groovy
@@ -11,7 +11,7 @@ pipeline {
             steps{
             sh """
             terraform init
-            terraform apply -var-file="dev.tfvars"
+            terraform apply -var-file="dev.tfvars" -auto-approve
             """
         }
         }
