4b26390cd86c153a562bcb29ec969c40be8ae378
mahesh <maheshryali@gmail.com>
2022-12-04 16:28:20 +0530

    addedcahnges

--- instance.tf
+++ instance.tf
@@ -7,29 +7,21 @@ resource "aws_instance" "web" {
   subnet_id                   =   aws_subnet.subnets[count.index].id 
   vpc_security_group_ids      =   [aws_security_group.forinstances.id] 
 
-  tags = {
-    Name = var.instance_names[count.index]
-  }
-}
-
-resource "null_resource" "fornginx" {
-  triggers = {
-    running_numbers = 1.0
-  }
-  connection {
+  provisioner "remote-exec" {
+    connection {
     type        =  "ssh"
     user        =  "ubuntu"
     private_key =  file("/home/ubuntu/id_rsa")
     host        =  aws_instance.web[0].public_ip
   }
-  provisioner "remote-exec" {
     inline = [
       "sudo apt update",
       "sudo apt install nginx -y"
     ]
   }
 
-  depends_on = [
-    aws_instance.web
-  ]
+  tags = {
+    Name = var.instance_names[count.index]
+  }
 }
+
